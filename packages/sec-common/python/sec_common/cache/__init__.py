509e7117c98409ef06353d895899c38d27aa9111
Nicholas-Arcari <arcari.nicholas0@gmail.com>
2026-04-26 12:02:16 +0200

    feat(sec-common): cache subpackage entrypoint for service-keyed API responses

---
+++ packages/sec-common/python/sec_common/cache/__init__.py
@@ -0,0 +1,9 @@
+from sec_common.cache.db import (
+    CacheEntry,
+    configure,
+    get_cached,
+    init_db,
+    set_cached,
+)
+
+__all__ = ["CacheEntry", "configure", "get_cached", "init_db", "set_cached"]
