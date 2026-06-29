8c1ad954fcbd29b8498b0c0c382577de692ac78c
Nicholas-Arcari <arcari.nicholas0@gmail.com>
2026-06-29 11:04:41 +0200

    refactor(bootstrap): wire ThemeProvider and i18n

--- packages/osint-toolkit/frontend/src/main.tsx
+++ packages/osint-toolkit/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider, RequireAuth } from "@sec-toolkit/common/auth";
+import { ThemeProvider } from "@sec-toolkit/common/theme";
 import App from "./App";
 import api from "./api/client";
 import "./index.css";
@@ -10,11 +11,13 @@ import "./i18n";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider client={api} scope="osint">
-        <RequireAuth loginProps={{ title: "OSINT Toolkit" }}>
-          <App />
-        </RequireAuth>
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider client={api} scope="osint">
+          <RequireAuth loginProps={{ title: "OSINT Toolkit" }}>
+            <App />
+          </RequireAuth>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
